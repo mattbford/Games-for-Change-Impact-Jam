d63ef6c761196e2894cc6228c27ae5be5aa39927
mattbford <mattbford@live.com>
2021-03-24 13:32:10 -0700

    boat like movement

--- Assets/Scripts/BoatHandler.cs
+++ Assets/Scripts/BoatHandler.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class BoatHandler : MonoBehaviour
 {
-    public float speed = 5;
+    public float accelerationFactor = 15f;
+    public float rotateSpeed = 1;
+
+    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Quaternion rotation = transform.rotation * Quaternion.Euler(0, horizontal * rotateSpeed, 0);
+
+        rb.AddForce(transform.forward * accelerationFactor * vertical, ForceMode.Acceleration);
+        rb.MoveRotation(rotation);
     }
 }
